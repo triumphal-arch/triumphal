a4ec8bbccf142e063d2ade56770a4b17803127e4
Peter <p3732@users.noreply.github.com>
2021-03-20 02:06:08 +0100

    rearrange files and drop python script

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Triumphal Arch Linux
-Main repository for Triumphal. Contains archiso profile and os-installer PKGBUILD
+Main repository for Triumphal. Contains archiso profile and os-installer PKGBUILD (for now).
 ## Create ISO
 ```script
 ./build_iso.sh
--- build_iso.sh
+++ build_iso.sh
@@ -1,25 +1,40 @@
 #!/bin/sh
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+# directory paths
+archiso_example_profile='/usr/share/archiso/configs/releng/'
+profile_dir=$(pwd)/archiso_profile
+build_dir=$(pwd)/build
+package_dir=$build_dir/packages
+
 # remove previous build
-sudo rm -rf build
+sudo rm -rf $build_dir
 sudo rm -rf triumphal-*
-sudo rm -rf /tmp/archiso_workdir
+#sudo rm -rf /tmp/archiso_workdir
 
 #for file creation flags
 umask 0022
 
-### prepare archiso ###
-sudo ./setup_archiso_profile.py
+# Create directories
+mkdir -p $package_dir
 
-# build installer
-pushd build/os-installer
-sudo chown -R $(whoami) .
-makepkg os-installer
-repo-add os-installer.db.tar.gz os-installer-*
-REPO_PATH=$(pwd)
-popd
-sudo sed -i s,@@REPO_PATH@@,$REPO_PATH, build/pacman.conf
+# Setup airottfs
+cp -a $archiso_example_profile $build_dir
+# remove unwanted, patch and add files from/to default profile.
+for line in $(cat $profile_dir/remove); do rm -rf $build_dir/$line; done
+for file in $(ls  $profile_dir/patch);  do cat $profile_dir/patch/$file >> $build_dir/$file; done
+for file in $(ls  $profile_dir/add);    do cp -a $profile_dir/add/$file $build_dir/; done
+
+### build packages ###
+
+package_name=os-installer
+pushd $build_dir/$package_name >> /dev/null
+makepkg $package_name
+mv $package_name-* $package_dir
+popd >> /dev/null
+
+repo-add $build_dir/triumphal.db.tar.gz $package_dir/*
+sudo sed -i s,@@REPO_PATH@@,$build_dir, $build_dir/pacman.conf
 
 ### prepare build system ###
 # create temporary folder
