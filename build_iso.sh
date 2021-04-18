270e2d8e36a69502070d1e3ab30983844ef1af7f
Peter <650-p3732@users.noreply.gitlab.gnome.org>
2021-04-18 02:13:05 +0200

    don't rebuild package if still same commit

--- build_iso.sh
+++ build_iso.sh
@@ -19,13 +19,21 @@ build_package () {
     if [ -e $package_dir ]
     then
         pushd $package_dir >> /dev/null
+        previous_commit=$(git rev-parse HEAD)
         git pull
+        current_commit=$(git rev-parse HEAD)
+        if [[ $previous_commit == $current_commit && ! $(find $database_dir -name $package_name*.tar) == '' ]]
+        then
+            # skip already build package
+            return
+        fi
     else
         git clone $git_package_url $package_dir
         pushd $package_dir >> /dev/null
     fi
+    packages_changed=true
     makepkg $package_name
-    mv $package_name-* $tarball_dir
+    mv $package_name-* $database_dir
     popd >> /dev/null
 }
 
@@ -53,15 +61,17 @@ profile_dir=$(pwd)/archiso_profile
 build_dir=$(pwd)/build
 build_profile_dir=$build_dir/profile
 database_dir=$build_dir/packages
-tarball_dir=$database_dir/tarballs
 work_dir='/tmp/archiso_workdir'
 installer_config=$(pwd)/installer_config
 installer_config_target=$build_profile_dir'/airootfs/etc/os-installer'
 # file paths
 swapfile=/swapfile_iso_build
+# other
+packages_changed=false
+
 
 ### Prepare for build ###
-# remove previous build stuf^
+# remove previous build stuff
 sudo rm -rf $build_profile_dir
 cleanup
 # flags for file creation
@@ -83,7 +93,6 @@ cp -a $installer_config/* $installer_config_target
 
 ### Build packages ###
 mkdir -p $database_dir
-mkdir -p $tarball_dir
 build_package os-installer git@github.com:p3732/os-installer-pkgbuild.git
 repo-add $database_dir/triumphal.db.tar.gz $tarball_dir/*.pkg.tar
 sudo sed -i s,@@REPO_PATH@@,$database_dir, $build_profile_dir/pacman.conf
